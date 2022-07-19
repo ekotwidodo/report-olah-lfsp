0cdb5ea7496cdde90d448af0483f1dac5d6fda69
Eko Teguh Widodo <id.ekoteguh@gmail.com>
2022-07-19 15:10:02 +0700

    Implementasi report rumah tangga(ruta)

---
+++ functions/report-ruta.php
@@ -0,0 +1,51 @@
+<?php
+
+    function findAll($connection)
+    {
+        $sql = "SELECT 
+                    t0.idbs as idbs, t0.nus as nus, t0.nama_krt as nama_krt, t1.nama_operator as nama_operator, t1.status_dok as status_dokumen, CAST(t1.waktu_entri AS DATE) as waktu_entri
+                FROM
+                    (SELECT 
+                        CONCAT(dsrt.kode_prov,dsrt.kode_kab,dsrt.kode_kec,dsrt.kode_desa,dsrt.nbs,'B') AS idbs, dsrt.no_dsrt as nus, dsrt.b3k6 as nama_krt
+                    FROM SP2020C2_Validasi.dbo.m_dsrt dsrt)t0
+                LEFT JOIN 
+                    (SELECT 
+                        CONCAT(rt.kode_prov,rt.kode_kab,rt.kode_kec,rt.kode_desa,rt.nbs,'B') AS idbs, rt.no_rt as nus, mo.realname as nama_operator, rt.status_dok as status_dok, rt.time_locked as waktu_entri
+                    FROM 
+                        SP2020C2_Validasi.dbo.C2_t_rt rt, SP2020C2_Validasi.dbo.m_operator mo
+                    WHERE 
+                        rt.kode_operator=mo.id_operator
+                    GROUP BY 
+                        rt.kode_prov,rt.kode_kab,rt.kode_kec,rt.kode_desa,rt.nbs,rt.no_rt, mo.realname, rt.status_dok, rt.time_locked)t1
+                ON t0.idbs=t1.idbs AND t0.nus=t1.nus
+                ORDER BY t0.idbs, t0.nus";
+        $results = $connection->get_rows_v2($sql);
+        return $results;
+    }
+
+    function findStatus($connection, $status, $kabkota)
+    {
+        $sql = "SELECT COUNT(*) AS jumlah
+                FROM
+                    (SELECT 
+                        t0.idbs as idbs, t0.nus as nus, t0.nama_krt as nama_krt, t1.nama_operator as nama_operator, t1.status_dok as status_dokumen, CAST(t1.waktu_entri AS DATE) as waktu_entri
+                    FROM
+                        (SELECT 
+                            CONCAT(dsrt.kode_prov,dsrt.kode_kab,dsrt.kode_kec,dsrt.kode_desa,dsrt.nbs,'B') AS idbs, dsrt.no_dsrt as nus, dsrt.b3k6 as nama_krt
+                        FROM SP2020C2_Validasi.dbo.m_dsrt dsrt)t0
+                    LEFT JOIN 
+                        (SELECT 
+                            CONCAT(rt.kode_prov,rt.kode_kab,rt.kode_kec,rt.kode_desa,rt.nbs,'B') AS idbs, rt.no_rt as nus, mo.realname as nama_operator, rt.status_dok as status_dok, rt.time_locked as waktu_entri
+                        FROM 
+                            SP2020C2_Validasi.dbo.C2_t_rt rt, SP2020C2_Validasi.dbo.m_operator mo
+                        WHERE 
+                            rt.kode_operator=mo.id_operator
+                        GROUP BY 
+                            rt.kode_prov,rt.kode_kab,rt.kode_kec,rt.kode_desa,rt.nbs,rt.no_rt, mo.realname, rt.status_dok, rt.time_locked)t1
+                    ON t0.idbs=t1.idbs AND t0.nus=t1.nus)t
+                WHERE 
+                    " . $status . " AND SUBSTRING(idbs, 1, 4)='" . $kabkota . "'";
+        $results = $connection->get_rows_v2($sql);
+        return $results;
+    }
+?>
--- ruta.php
+++ ruta.php
@@ -1,8 +1,35 @@
 <?php
+
+require './vendor/autoload.php';
 include './config/database.php';
-include './functions/report-operator.php';
+include './functions/report-ruta.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__); //Notice the Namespace and Class name
+$dotenv->load();
+
 
 $c1 = new Database();
+$r1 = findAll($c1);
+$c1->close();
+
+$c2 = new Database();
+$r2 = findStatus($c2, "status_dokumen='C'", '1801');
+$c2->close();
+$c3 = new Database();
+$r3 = findStatus($c3, "status_dokumen='C'", '1813');
+$c3->close();
+$c4 = new Database();
+$r4 = findStatus($c4, "status_dokumen='E'", '1801');
+$c4->close();
+$c5 = new Database();
+$r5 = findStatus($c5, "status_dokumen='E'", '1813');
+$c5->close();
+$c6 = new Database();
+$r6 = findStatus($c6, "status_dokumen IS NULL", '1801');
+$c6->close();
+$c7 = new Database();
+$r7 = findStatus($c7, "status_dokumen IS NULL", '1813');
+$c7->close();
 ?>
 
 <!DOCTYPE html>
@@ -12,7 +39,7 @@ $c1 = new Database();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Progress Report</title>
+        <title>Progress Report | Rumah Tangga</title>
         <link rel="shortcut icon" href="https://bpslampung.id/landing/img/logo.png" type="image/png">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
@@ -20,6 +47,13 @@ $c1 = new Database();
 
 <body>
         <div class="container">
+                <div class="row">
+                        <div class="col m-3 ml-auto">
+                                <a href="index.php" class="btn btn-success">Lihat Operator</a>
+                                <a href="#" class="btn btn-danger">Lihat Rekap Tipe Kesalahan</a>
+                                <a href="#" class="btn btn-warning">Evaluasi Data</a>
+                        </div>
+                </div>
                 <div class="row">
                         <h1 class="text-center">Report Ruta Hasil Pengolahan C2 dan V</h1>
                         <h5 class="text-center">Kondisi: <?php echo date('d-m-Y')?></h5>
@@ -29,9 +63,10 @@ $c1 = new Database();
                         <div class="col">
                                 <h6>BPS Kabupaten Lampung Barat</h6>
                                 <ul>
-                                        <li>Jumlah Ruta Belum Olah:</li>
-                                        <li>Jumlah Ruta Clean Olah:</li>
-                                        <li>Jumlah Ruta Error Olah:</li>
+                                        <li>Jumlah Ruta Belum Olah: <?php echo $r6[0]['jumlah']; ?></li>
+                                        <li>Jumlah Ruta Clean Olah: <?php echo $r2[0]['jumlah']; ?></li>
+                                        <li>Jumlah Ruta Error Olah: <?php echo $r4[0]['jumlah']; ?></li>
+                                        <li>Jumlah Ruta Total: <?php echo $r2[0]['jumlah'] + $r4[0]['jumlah'] + $r6[0]['jumlah']; ?></li>
                                 </ul>
                         </div>
                         <div class="col">
@@ -40,9 +75,10 @@ $c1 = new Database();
                         <div class="col">
                                 <h6>BPS Kabupaten Pesisir Barat</h6>
                                 <ul>
-                                        <li>Jumlah Ruta Belum Olah:</li>
-                                        <li>Jumlah Ruta Clean Olah:</li>
-                                        <li>Jumlah Ruta Error Olah:</li>
+                                        <li>Jumlah Ruta Belum Olah: <?php echo $r7[0]['jumlah']; ?></li>
+                                        <li>Jumlah Ruta Clean Olah: <?php echo $r3[0]['jumlah']; ?></li>
+                                        <li>Jumlah Ruta Error Olah: <?php echo $r5[0]['jumlah']; ?></li>
+                                        <li>Jumlah Ruta Total: <?php echo $r3[0]['jumlah'] + $r5[0]['jumlah'] + $r7[0]['jumlah']; ?></li>
                                 </ul>
                         </div>
                 </div>
@@ -55,13 +91,31 @@ $c1 = new Database();
                                         <th class="text-center">No.</th>
                                         <th class="text-center">IDBS</th>
                                         <th class="text-center">NUS</th>
+                                        <th class="text-center">Nama KRT</th>
                                         <th class="text-center">Operator</th>
-                                        <th class="text-center">Status (Belum)</th>
-                                        <th class="text-center">Status (Clean)</th>
-                                        <th class="text-center">Status (Error)</th>
+                                        <th class="text-center">Waktu Entri</th>
+                                        <th class="text-center">Status Dokumen</th>
                                 </tr>
                         </thead>
                         <tbody>
+                                <?php if (count($r1) > 0) {
+                                        $i = 0;
+                                        foreach ($r1 as $list => $row) {
+                                
+                                ?>   
+                                <tr <?php echo ($row['status_dokumen']==NULL || $row['status_dokumen']=='') ? 'style="background-color: #FFB4B4"' : '' ?> >
+                                        <td class="text-center"><?php echo ($i + 1) ?></td>
+                                        <td class="text-center"><?php echo $row['idbs'] ?></td>
+                                        <td class="text-center"><?php echo $row['nus'] ?></td>
+                                        <td><?php echo $row['nama_krt'] ?></td>
+                                        <td><?php echo $row['nama_operator'] ?></td>
+                                        <td class="text-center"><?php echo $row['waktu_entri'] ?></td>
+                                        <td class="text-center"><?php echo $row['status_dokumen'] ?></td>
+                                </tr> 
+                                        <?php 
+                                        $i++;
+                                        } ?>
+                                <?php } ?>
                         </tbody>
                 </table>
         </div>
